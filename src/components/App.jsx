c7ba2f9e4bf8ce52ff4a24a600aafbb998bef5c0
aleksey-prokopchuk <prokop4huk@ukr.net>
2022-09-16 13:28:14 +0300

    Пофіксив стилі в App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,12 @@ export const App = () => {
   return (
     <div
       style={{
-        padding: 20,
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
       }}
     >
       <Section title="Please leave feedback" />
